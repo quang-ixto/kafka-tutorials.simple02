e413abf21fc03ceb4056625e4c963986d2a2262e
Quang Nhan Nguyen <qnguyen@ixto.de>
2021-05-21 13:30:40 +0200

    fixed package name

--- src/main/java/com.example.kafka.simple02/Producer.java
+++ src/main/java/com.example.kafka.simple02/Producer.java
@@ -1,4 +1,4 @@
-package kafkatutorials.producerconsumer.simple02;
+package com.example.kafka.simple02;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
--- src/main/java/com.example.kafka.simple02/Simple02Application.java
+++ src/main/java/com.example.kafka.simple02/Simple02Application.java
@@ -1,4 +1,4 @@
-package kafkatutorials.producerconsumer.simple02;
+package com.example.kafka.simple02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
